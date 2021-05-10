60f4dc120c297423f6de4eaddb1fdfce70aab0f1
Avi Stramer <astramer@gmail.com>
2021-05-10 15:01:08 -0500

    log perf log

--- src/main/java/io/testable/selenium/TestableExample.java
+++ src/main/java/io/testable/selenium/TestableExample.java
@@ -2,6 +2,12 @@ package io.testable.selenium;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.logging.LogEntries;
+import org.openqa.selenium.logging.LogEntry;
+import org.openqa.selenium.logging.LogType;
+import org.openqa.selenium.logging.LoggingPreferences;
+
+import java.util.logging.Level;
 
 /**
  * Simple example of loading Google and taking a screenshot in a way that is compatible with the Testable platform.
@@ -9,10 +15,19 @@ import org.openqa.selenium.chrome.ChromeOptions;
 public class TestableExample {
 
     public static void main(String[] args) {
+        LoggingPreferences logPrefs = new LoggingPreferences();
+        logPrefs.enable(LogType.PERFORMANCE, Level.ALL);
         ChromeOptions options = new ChromeOptions();
+        options.setCapability("goog:loggingPerfs", logPrefs);
         WebDriver driver = TestableSelenium.newWebDriver(options);
         driver.get("https://www.google.com");
         TestableSelenium.takeScreenshot(driver, "temp.png");
+
+        LogEntries logEntries = driver.manage().logs().get(LogType.PERFORMANCE);
+        for (LogEntry entry : logEntries) {
+            System.out.println(entry.toString());
+        }
+
         driver.close();
     }
 
